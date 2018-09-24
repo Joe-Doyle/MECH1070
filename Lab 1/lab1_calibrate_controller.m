4e0496a5e0d5ba1b0df2fbc595c348495e9dba6f
Joseph Doyle <j@random-stuff.org>
2018-09-23 22:11:33 -0400

    initial commit

---
+++ Lab 1/lab1_calibrate_controller.m
@@ -0,0 +1,25 @@
+try
+        s = serial('/dev/tty.usbmodem1461')
+        set(s,'BaudRate',115200)
+        fopen(s)
+        % Set the new values of the parameters
+        fprintf (s, '$100 = 157.480')
+        pause(1)
+        fprintf (s, '$101 = 157.480')
+        pause(1)
+        fprintf (s, '$102 = 157.480')
+        pause(1)
+        % Set the new values of the parameters
+        fprintf (s, '$120 = 15.748')
+        pause(1)
+        fprintf (s, '$121 = 15.748')
+        pause(1)
+        fprintf (s, '$122 = 15.748')
+        pause(1)
+        % Close the connection to the GRBL controller
+        fclose(s)
+catch ME
+    fclose(s)
+    ME.message
+end
+
