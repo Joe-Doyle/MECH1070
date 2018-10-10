a40b13f813bc9429e953533d233c7e8320900b52
Joseph Doyle <j@random-stuff.org>
2018-10-09 23:23:37 -0400

    function to compute beam deflection works

---
+++ Lab 2/calcDef.m
@@ -0,0 +1,10 @@
+function deflection = calcDef(L, F, E, b, h)
+
+%This function calculates the deflection of a beam using
+%(L)ength, (F)Point Load, (E)Youngs Modulus in GPa, (b)width of
+%beam cross section, and (h)eight of beam cross section.
+
+I = (b*h^3)/12 %compute second moment of area from beam width and height
+E = E*145037.73 %convert GPa to psi
+deflection = (F*L^3)/(48*E*I) %compute deflection
+end
