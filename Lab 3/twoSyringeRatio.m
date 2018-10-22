314246a8f82a1bcc9da45b387c53b1c9fb5d646d
Joseph Doyle <j@random-stuff.org>
2018-10-22 14:10:19 -0400

    two syringes

--- Lab 3/twoSyringeRatio.m
+++ Lab 3/twoSyringeRatio.m
@@ -1,23 +1,14 @@
 try
-    % First we will setup a serial port 
-    % associated with the controller USB ? Change the 
-    % COM port description in the single quotes as 
-    % needed. 
+    %com setup and initialization
     s = serial('/dev/tty.usbmodem14401')
-    % Next we will setup the port communication speed
-    % 	This may require some trial and error, but 
-    %	115200 appears to be a good value (9600 may 
-    % 	work too)
     set(s,'BaudRate',115200)
-	% Now we actually open the serial port
     fopen(s)
-    % We tell matlab to pause/wait  for 2 seconds to
-    % make sure the controller connects to the
-    % computer
     pause(2)
+    
 	% Now we start sending commands to the controller 
     % using the ?print? function 
     fprintf (s, 'G17 G20 G90 G94 G54')
+    
     % The following commands simply move the motor to
     % a new x,y,z position with a feedrate = F.
     fprintf (s,'G1 x0.0917 y0.3668 F5') %split 0.4585 into whatever ratio needed
