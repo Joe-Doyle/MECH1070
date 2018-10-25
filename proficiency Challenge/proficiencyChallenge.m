6cd75679fb2441484eaadeb6fa2d0331613737ce
Joseph Doyle <j@random-stuff.org>
2018-10-25 14:47:26 -0400

    added user input functionality for relative or absolute positioning and number of desired clicks

--- proficiency Challenge/proficiencyChallenge.m
+++ proficiency Challenge/proficiencyChallenge.m
@@ -1,4 +1,12 @@
 try
+    %user input of absolute or relative positioning
+    prompt = 'Enter 1 for relative positioning and 0 for absolute ';
+    isRel  = input(prompt)
+    
+    %user input of number of desired clicks
+    prompt = 'Enter a positive integer of how many clicks you want ';
+    numClk = input(prompt)
+    
     %set up serial
     s = serial('/dev/tty.usbmodem3595310')
     set(s,'BaudRate',115200)
@@ -17,19 +25,19 @@ try
     plot(xscale2,yscale2,'g+')
     ppi     = xscale2 - xscale1
     zero    = xscale1
-    relpos  = 0
+    relPos  = 0
     
     %configure CNC controller box
     fprintf (s, 'G17 G20 G90 G94 G54')
     
-    %loop 15 times to allow 15 position clicks on image
-    for i = 1:15
-        xmove = 0
+    %loop for user inputted number of clicks
+    for i = 1:numClk
+        xmove = 0;
         %click on scale to acquire point to move
         [xpos,ypos] = ginput(1)
     
         %scale point to vector from 0
-        xmove = xpos - zero
+        xmove = xpos - zero;
         xmove = xmove/ppi
     
         %limit movement to +2"
@@ -39,10 +47,10 @@ try
         if(xmove < -2); xmove = -2; end;
     
         %account for automatic absolute positioning
-        relpos = relpos + xmove
+        if(isRel), relPos = relPos + xmove; else, relPos = xmove; end
         
         %create G code string
-        CNCMotion = ['G1 x', num2str(relpos),' F10']
+        CNCMotion = ['G1 x', num2str(relPos,4),' F10']
         
         %print G code string to axis control box
         fprintf (s,CNCMotion)
