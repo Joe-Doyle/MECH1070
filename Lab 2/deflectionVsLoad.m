ec1706c62f56645f7daf269c456245d4783801cf
Joseph Doyle <j@random-stuff.org>
2018-10-25 15:05:55 -0400

    variable name changes to reflect ability to compute for different materials

--- Lab 2/deflectionVsLoad.m
+++ Lab 2/deflectionVsLoad.m
@@ -3,23 +3,24 @@
 %the deflection formula and the material's young's modulus
 
 try
+    %enter dimensions and modulus of material
     len     = 5     %beam length in inches
     width   = 3/4   %beam profile width in inches
     height  = 3/16  %beam profile height in inches
-    pineMod = 9     %Young's modulus for pine in GPa
+    modulus = 9     %Young's modulus for material in GPa
     
-    %generate vector of load values in pounds
-    load    = 2.0:2:52.0
-    
-    %generate vector of measured deflection in inches
+    %enter vector of measured deflection in inches
     meDef   = [.0089 .0139 .0190 .0246 .0295 .0341 .0394 .0442 .0507 ...
         .0548 .0605 .0653 .0711 .0770 .0834 .0890 .0958 .1031 .1125 ...
         .1232 .1330 .1410 .1505 .1665 .1776 .2032]
-
+    
+    %enter vector of load values in pounds
+    load    = 2.0:2:52.0
+    
     %generate vector of theoretical load values from young's modulus and the
     %deflection calculator function
     for i=1:length(load)
-        theDef(i) = calcDef(len,load(i),pineMod,width,height)
+        theDef(i) = calcDef(len,load(i),modulus,width,height)
     end
         
     %plot measured and calculated deflection vs load
