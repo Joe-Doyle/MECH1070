134c3b69b76f8251ec440f3b75445d2e2088145c
Joseph Doyle <j@random-stuff.org>
2018-10-24 21:57:58 -0400

    Added functionality and cleaned up two syringe ratio, now input is in percent with no required pre-calculation

--- Lab 3/twoSyringeRatio.m
+++ Lab 3/twoSyringeRatio.m
@@ -1,19 +1,29 @@
 try
-    %com setup and initialization
+    %system config block
+    red     = 80        %percent of red solution
+    blu     = 20        %percent of blue solution
+    totDist = 0.4585    %total distance one syringe needs to be displaced for the desired volume
+    
+    %set up serial
     s = serial('/dev/tty.usbmodem14401')
     set(s,'BaudRate',115200)
     fopen(s)
     pause(2)
     
-	% Now we start sending commands to the controller 
-    % using the ?print? function 
+	%G-Code setup
     fprintf (s, 'G17 G20 G90 G94 G54')
     
-    % The following commands simply move the motor to
-    % a new x,y,z position with a feedrate = F.
-    fprintf (s,'G1 x0.0917 y0.3668 F5') %split 0.4585 into whatever ratio needed
-    display('All Done!!!')
-    % Close the connection to the GRBL controller
+    %calculate relative movement of the axis for the desired percentages
+    redMove = red/100*totDist
+    bluMove = blu/100*totDist   
+    
+    %create G-Code string
+    CNCMotion = ['G1 x', num2str(redMove),' y', num2str(bluMove), ' F10']
+    
+    %print G-Code string to controller
+    fprintf (s,CNCMotion) 
+    
+    % Close the connection to the controller
     fclose(s)
 catch ME
 	fclose(s)
